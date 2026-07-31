4b9310f8adf4c4adccee600e8c740c7911dc0c3b
eaaa50028 <eaaa50028@gmail.com>
2026-07-31 09:33:55 -0400

    tareas de programacion

---
+++ Ejercicios/suma_de_numeros.py
@@ -0,0 +1,11 @@
+#Solicita dos numeros enteros. Calcular la suma y mostrar si el resultado es mayor a 100
+
+numero1 = int(input("Ingresa un numero entero: "))
+numero2= int(input("Ingresa otro numero entero: "))
+
+suma = numero1 + numero1
+
+if suma > 100 :
+    print("La suma es mayor que 100")
+else :
+    print("La suma es menor que 100")
